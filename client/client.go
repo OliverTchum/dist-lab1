7f62a16c656d7fab678fed2d63f6a44a2bb59b0f
Oliver Tchum <iy23823@bristol.ac.uk>
2024-10-03 15:59:53 +0100

    finished chat server

--- client/client.go
+++ client/client.go
@@ -1,24 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"flag"
-	"net"
 	"fmt"
+	"net"
+	"os"
 )
 
 func read(conn net.Conn) {
 	//TODO In a continuous loop, read a message from the server and display it.
+	for {
+		reader := bufio.NewReader(conn)
+		msg, _ := reader.ReadString('\n')
+		fmt.Printf(msg)
+	}
 }
 
 func write(conn net.Conn) {
 	//TODO Continually get input from the user and send messages to the server.
+	stdin := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Printf("enter message: ")
+		msg, _ := stdin.ReadString('\n')
+		fmt.Fprintln(conn, msg)
+	}
 }
 
 func main() {
 	// Get the server address and port from the commandline arguments.
 	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
 	flag.Parse()
+	ln, _ := net.Listen("tcp", *addrPtr)
+	readconn, _ := ln.Accept()
+	conn, _ := net.Dial("tcp", *addrPtr)
+
 	//TODO Try to connect to the server
 	//TODO Start asynchronously reading and displaying messages
 	//TODO Start getting and sending user messages.
+
+	go read(readconn)
+	go write(conn)
+	for {
+	}
 }
--- server/server.go
+++ server/server.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"flag"
-	"net"
 	"fmt"
+	"net"
 )
 
 type Message struct {
@@ -21,6 +21,8 @@ func acceptConns(ln net.Listener, conns chan net.Conn) {
 	// TODO: all
 	// Continuously accept a network connection from the Listener
 	// and add it to the channel for handling connections.
+	conn, _ := ln.Accept()
+	conns <- conn
 }
 
 func handleClient(client net.Conn, clientid int, msgs chan Message) {
@@ -29,6 +31,13 @@ func handleClient(client net.Conn, clientid int, msgs chan Message) {
 	// Read in new messages as delimited by '\n's
 	// Tidy up each message and add it to the messages channel,
 	// recording which client it came from.
+	stdin := bufio.NewReader(client)
+	for {
+		msg, _ := stdin.ReadString('\n')
+		newmessage := Message{clientid, msg}
+		msgs <- newmessage
+
+	}
 }
 
 func main() {
@@ -36,7 +45,7 @@ func main() {
 	// Default to port 8030
 	portPtr := flag.String("port", ":8030", "port to listen on")
 	flag.Parse()
-
+	ln, _ := net.Listen("tcp", *portPtr)
 	//TODO Create a Listener for TCP connections on the port given above.
 
 	//Create a channel for connections
@@ -48,6 +57,7 @@ func main() {
 
 	//Start accepting connections
 	go acceptConns(ln, conns)
+	clientID := 0
 	for {
 		select {
 		case conn := <-conns:
@@ -55,9 +65,19 @@ func main() {
 			// - assign a client ID
 			// - add the client to the clients map
 			// - start to asynchronously handle messages from this client
+			go handleClient(conn, clientID, msgs)
+			clients[clientID] = conn
+			clientID++
 		case msg := <-msgs:
 			//TODO Deal with a new message
 			// Send the message to all clients that aren't the sender
+			for i, c := range clients {
+				if i != msg.sender {
+					fmt.Fprintln(c, msg.message)
+				}
+
+			}
+
 		}
 	}
 }
